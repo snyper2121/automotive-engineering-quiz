c865c331ec4617f264c28de7f741b12464500a80
Gabriel <gaby4b11@gmail.com>
2016-09-27 11:08:35 -0400

    Add files via upload
    Updated app.js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+   // console.log( "ready!" );
+   	var score = 0;
+   	var currentQuestion = 1;
+    var correctAnswers = [
+   	"Uneven tire wear",
+   	"10:1",
+   	"Continually recharge the battery",
+   	"Distributes intake air equally to the cylinders",
+   	"14.7:1"
+];
+
+	$('#q1 .answers').click(function(event) {
+		var answer = $('p', this).text();
+});
+
 });
 
 
@@ -25,66 +39,59 @@ $( document ).ready(function() {
 
 
 
+// var numArray = [1,3,5,7,5];
+// var someString = "Hello World";
 
+// var quizgame = {
+// 	question1: "What color is the sky?"
+// };
 
+// console.log(quizgame.question1);
+// console.log(quizgame['question1']);
 
+// var questionObject = {
+// 	question: "What is the effect of having too much camber in the tires?",
+// 	a:"Too much traction",
+//   b:"Uneven tire wear",
+//   c:"Hard Steering",
+//   d:"Excessive steering alignment torque",
+// 	rightAnswer: "b",
+//   keyName: "valuename",
+//   questionNumber: 30
+// };
 
+// var questionArray = [{},{},{},{}];
+// questionArray.push();
+// questionArray.pop();
+// questionArray.length;
+// questionArray[4];
+// [10,20,30,40,50];
 
+// questionArray[0] === 10;
+// questionArray.length = 5;
 
+// [[1,3,2],[1,2],[1,1]]
+// var playerArray = [{player: "George",health: 500},{player: "Sam",health: 200}]
 
-var numArray = [1,3,5,7,5];
-var someString = "Hello World";
+// var questionArray = [{question: "Who?"}, {question: "What?"}]
 
-var quizgame = {
-	question1: "What color is the sky?"
-};
+// question[0].question
+// question[1].question
+// question[1].a
+// question[2].rightAnswer = ;
 
-console.log(quizgame.question1);
-console.log(quizgame['question1']);
+// playerArray[1] => { player: "Sam", health: 200}
 
-var questionObject = {
-	question: "What is the effect of having too much camber in the tires?",
-	a:"Too much traction",
-  b:"Uneven tire wear",
-  c:"Hard Steering",
-  d:"Excessive steering alignment torque",
-	rightAnswer: "b",
-  keyName: "valuename",
-  questionNumber: 30
-};
+// playerArray[1].player
 
-var questionArray = [{},{},{},{}];
-questionArray.push();
-questionArray.pop();
-questionArray.length;
-questionArray[4];
-[10,20,30,40,50];
+// playerArray[1]['player'];
 
-questionArray[0] === 10;
-questionArray.length = 5;
+// var score = 0;
 
-[[1,3,2],[1,2],[1,1]]
-var playerArray = [{player: "George",health: 500},{player: "Sam",health: 200}]
-
-var questionArray = [{question: "Who?"}, {question: "What?"}]
-
-question[0].question
-question[1].question
-question[1].a
-question[2].rightAnswer = ;
-
-playerArray[1] => { player: "Sam", health: 200}
-
-playerArray[1].player
-
-playerArray[1]['player'];
-
-var score = 0;
-
-if ( userGuess === questionObject.rightAnswer ){
-	score += increment;
-	advanceGameToNextQuestion();
-}
+// if ( userGuess === questionObject.rightAnswer ){
+// 	score += increment;
+// 	advanceGameToNextQuestion();
+// }
 
 
 
