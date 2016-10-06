9220ec15fb1daff85faa40d251fbd99a9d89469b
snyper2121 <gaby4b11@gmail.com>
2016-10-06 16:53:18 -0400

    Quiz app almost finished. Waiting for revision.

--- js/app.js
+++ js/app.js
@@ -1,102 +1,132 @@
 $( document ).ready(function() {
    // console.log( "ready!" );
-   	var score = 0;
-   	var currentQuestion = 1;
-    var correctAnswers = [
-   	"Uneven tire wear",
-   	"10:1",
-   	"Continually recharge the battery",
-   	"Distributes intake air equally to the cylinders",
-   	"14.7:1"
+   	
+	var questionList = [
+   	"What is the effect of having too much camber in the tires?",
+   	"If the air to fuel mixture in a spark ignition engine is too rich, then air to fuel ratio is about",
+   	"The function of an alternator in an automobile is to:",
+   	"The main function of the intake manifold is that it",
+   	"What is the ideal air to fuel ratio (AFR) in a conventional gas engine?"
 ];
+	
+	var questionArray = [
+	{
+		question:"What is the effect of having too much camber in the tires?",
+		a:"Too much traction",
+	  	b:"Uneven tire wear",
+	  	c:"Hard Steering",
+	  	d:"Excessive steering alignment torque",
+		rightAnswer: "b",
+	  	keyName: "valuename",
+	  	questionNumber: 1
+	  	//image: //write the image url for each one
+	},
+	{
+		question:"If the air to fuel mixture in a spark ignition engine is too rich, then air to fuel ratio is about",
+		a:"15:1",
+	  	b:"13:1",
+	  	c:"17:1",
+	  	d:"10:1",
+		rightAnswer: "d",
+	  	keyName: "valuename",
+	  	questionNumber: 2
+	},
+	{
+		question:"The function of an alternator in an automobile is to:",
+		a:"Partly convert engine power into electric power",
+	  	b:"Continually recharge the battery",
+	  	c:"Supply electric power",
+	  	d:"Convert mechanical energy into electrical energy",
+		rightAnswer: "b",
+	  	keyName: "valuename",
+	  	questionNumber: 3
+	},
+	{
+		question:"The main function of the intake manifold is that it",
+		a:"Distribute intake air equally to the cylinders",
+	  	b:"Promotes the mixture of air and fuel",
+	  	c:"Reduces intake noise",
+	  	d:"Cools the intake air to a suitable temperature",
+		rightAnswer: "a",
+	  	keyName: "valuename",
+	  	questionNumber: 4
+	},
+	{
+		question:"What is the ideal air to fuel ratio (AFR) in a conventional gas engine?",
+		a:"12.3:1",
+	  	b:"15.5:2",
+	  	c:"14.7:1",
+	  	d:"10.5:1",
+		rightAnswer: "c",
+	  	keyName: "valuename",
+	  	questionNumber: 5
+	}
+	];
 
-	$('#q1 .answers').click(function(event) {
-		var answer = $('p', this).text();
+	function loadQuestionAnswersForIndex(index) {
+		if (index > questionArray.length -1) {
+			return "This index does not exist for this array.";
+		}
+		document.getElementById('firstQuestion').innerHTML = questionArray[index].question;
+		document.getElementById('a').innerHTML = questionArray[index].a;
+		document.getElementById('b').innerHTML = questionArray[index].b;
+		document.getElementById('c').innerHTML = questionArray[index].c;
+		document.getElementById('d').innerHTML = questionArray[index].d;
+		document.getElementById('current-question-number').innerHTML = questionArray[index].questionNumber;
+};
+		
+	var gameState = {
+		score: 0,
+   		currentQuestion: 1,
+	}
+
+	loadQuestionAnswersForIndex(0);
+
+
+	$("#answerOptions").click(function(event) {
+		console.log(event);
+		checkNewAnswer(event);
+		advanceToNextQuestion();
+	}); 
+
+
+	function checkNewAnswerAndAdvance() {
+		console.log(gameState);
+		if(gameState.currentQuestion > 5) {
+			console.log('You have finished the game!');
+			return;
+			
+		}
+
+		else if(event.target.id === questionArray[gameState.currentQuestion].rightAnswer) {
+			gameState.score = gameState.score + 20;
+		} 
+
+			document.getElementById('score-number').innerHTML = gameState.score;
+
+
+			gameState.currentQuestion = gameState.currentQuestion + 1;
+			
+
+			loadQuestionAnswersForIndex(gameState.currentQuestion);
+	}
+
+	function checkNewAnswer(event) {
+
+		if(event.target.id === questionArray[gameState.currentQuestion - 1].rightAnswer){
+			gameState.score = gameState.score + 20;
+			document.getElementById('score-number').innerHTML = gameState.score;
+		}
+	}
+
+	function advanceToNextQuestion() {
+		if(gameState.currentQuestion < 5){
+			gameState.currentQuestion = gameState.currentQuestion + 1;
+			loadQuestionAnswersForIndex(gameState.currentQuestion -1);
+		} else {
+			$('body').html('<div class="centeredEnding">You have finished the game!<br><br>' + gameState.score + ' points achieved!</div>');
+		}
+	}
+
+
 });
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var numArray = [1,3,5,7,5];
-// var someString = "Hello World";
-
-// var quizgame = {
-// 	question1: "What color is the sky?"
-// };
-
-// console.log(quizgame.question1);
-// console.log(quizgame['question1']);
-
-// var questionObject = {
-// 	question: "What is the effect of having too much camber in the tires?",
-// 	a:"Too much traction",
-//   b:"Uneven tire wear",
-//   c:"Hard Steering",
-//   d:"Excessive steering alignment torque",
-// 	rightAnswer: "b",
-//   keyName: "valuename",
-//   questionNumber: 30
-// };
-
-// var questionArray = [{},{},{},{}];
-// questionArray.push();
-// questionArray.pop();
-// questionArray.length;
-// questionArray[4];
-// [10,20,30,40,50];
-
-// questionArray[0] === 10;
-// questionArray.length = 5;
-
-// [[1,3,2],[1,2],[1,1]]
-// var playerArray = [{player: "George",health: 500},{player: "Sam",health: 200}]
-
-// var questionArray = [{question: "Who?"}, {question: "What?"}]
-
-// question[0].question
-// question[1].question
-// question[1].a
-// question[2].rightAnswer = ;
-
-// playerArray[1] => { player: "Sam", health: 200}
-
-// playerArray[1].player
-
-// playerArray[1]['player'];
-
-// var score = 0;
-
-// if ( userGuess === questionObject.rightAnswer ){
-// 	score += increment;
-// 	advanceGameToNextQuestion();
-// }
-
-
-
-
-
-
-
-
